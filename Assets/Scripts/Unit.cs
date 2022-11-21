e10d465be17f6f5643dca8a1756b924a8a15a7ac
JacoboOrozco <orozcojacobo1@gmail.com>
2022-11-21 15:02:20 -0500

    Walking Animation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,14 +7,22 @@ public class Unit : MonoBehaviour
     private Vector3 targetPosition;
     [SerializeField]private MouseWorld mouseWorld;
 
+    [SerializeField]private Animator unitAnimator;
+
     private void Update()
     {
+        
         float stoppingDistance = .1f;
         if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
         if(Input.GetKeyDown(KeyCode.Mouse0))
