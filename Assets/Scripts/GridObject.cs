36c086c8caca0acc5b02240fba257d7d7157e7bf
OrozcoJacobo <orozcojacobo1@gmail.com>
2022-11-22 17:27:14 -0500

    Grid Display Coordinates
    Added a visual element in order to display information about the grids on each one, however is displaying just random numbers, next fix will address that problem and give accurate data to each grid

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,11 @@
+public class GridObject
+{
+    private GridPosition gridPosition;
+    private GridSystem gridSystem;
+
+    public GridObject( GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridPosition = gridPosition;
+        this.gridSystem = gridSystem;
+    }   
+}
